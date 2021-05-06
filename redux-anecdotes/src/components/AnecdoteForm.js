1ece3861294088e200d45b338ebcf36e295ef8ca
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-05-06 22:19:49 +0100

    added connection to database via json-server

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { newAnecdote } from "../reducers/anecdoteReducer";
+import { createNewAnecdote } from "../reducers/anecdoteReducer";
 import {
   createNotification,
   removeNotification,
 } from "../reducers/notificationReducer";
+import anecdoteService from "../services/anecotes";
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
-    dispatch(newAnecdote(content));
+    const newAnecdote = await anecdoteService.createAnecdote(content);
+    dispatch(createNewAnecdote(newAnecdote));
     dispatch(createNotification(`${content} was created`));
     setTimeout(() => {
       dispatch(removeNotification());
